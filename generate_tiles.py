56dd2ed0be7158e07caa4b829e8cb09ea30f599f
George Watkins <gwwatkin@sfu.ca>
2023-11-18 08:58:43 -0800

    Demo 2

--- generate_tiles.py
+++ generate_tiles.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import random
 
-def generate_grainy_image(base_color, width, height, filename):
+def generate_grainy_image(base_color, width, height, transparency, filename):
     image = Image.new('RGBA', (width, height), base_color)
     pixels = image.load()
 
@@ -9,12 +9,14 @@ def generate_grainy_image(base_color, width, height, filename):
         for j in range(height):
             r, g, b = base_color
             rand_variation = random.randint(-20, 20)  # Random variation for grainy effect
+            alpha = 0
+            if random.randint(0, transparency)==0 or (i == width-1 or i == 0 or j == height-1 or j == 0):
+                alpha = 255
+
             pixels[i, j] = (max(min(r + rand_variation, 255), 0), 
                             max(min(g + rand_variation, 255), 0), 
                             max(min(b + rand_variation, 255), 0),
-                            128 if random.randint(0, 4)==0 or (
-                                i == width-1 or i == 0 or j == height-1 or j == 0
-                            ) else 0)
+                            alpha)
 
     image.save(filename)
 
@@ -29,6 +31,7 @@ base_colors = [
 
 if __name__ == '__main__':
     for i, color in enumerate(base_colors):
-        generate_grainy_image(color, 16, 16, f'textures/routing_{i+1}.png')
-    generate_grainy_image(distillation_color, 16, 16, f'textures/distillation.png')
-    generate_grainy_image(qubit_color, 16, 16, f'textures/qubit.png')
+        generate_grainy_image(color, 16, 16, 0, f'textures/routing_{i+1}.png')
+    generate_grainy_image(distillation_color, 16, 16, 0, f'textures/distillation.png')
+    generate_grainy_image(base_colors[0], 16, 16, 10, f'textures/dead.png')
+    generate_grainy_image(qubit_color, 16, 16, 0, f'textures/qubit.png')
--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ ie = minetest.request_insecure_environment()
 
 local function insecure_load_file()
     local mod_path = minetest.get_modpath("latticesurgery")
-    local json_file_path = mod_path .. "/demo.json"
+    local json_file_path = mod_path .. "/demo2.json"
     f = ie.io.open(json_file_path)
     s = f:read("a")
     ie.io.close(f)
@@ -28,19 +28,57 @@ local function sleep(n)
     ie.os.execute("sleep " .. tonumber(n))
 end
 
+local function array_to_s(a)
+    local r = ""
+    for i, k in pairs(a) do
+        r = r .. k
+    end
+    return r
+end
+
+local function stritch_border(border, patch_type)
+    if border == "AncillaJoin" then return true end
+    if border == "SolidStiched" then return true end
+    if border == "DashedStiched" then return true end
+    if border == "Solid" then return false end
+    if border == "Dashed" then return false end
+    if border == "None" and patch_type == "DistillationQubit" then return true end
+
+    return false
+end
+
+local function is_dead_cell(cell)
+    if cell['patch_type'] == "Ancilla" and
+        cell['edges']['Top'] == "None" and
+        cell['edges']['Bottom'] == "None" and
+        cell['edges']['Left'] == "None" and
+        cell['edges']['Right'] == "None" then
+        return true
+    end
+    return false
+end
 
 local function place_layer(starting_point, slices)
     for t = 1, #slices do
         for r = 1, #slices[t] do
             for c = 1, #slices[t][r] do
                 local value = slices[t][r][c]
-                if value then
-                        local name = string.format("latticesurgery:routing_%i",t%12+1)
-                        if value['patch_type'] == 'DistillationQubit' then
-                            name = "latticesurgery:distillation"
-                        elseif value['patch_type'] == 'Qubit' then
-                            name = "latticesurgery:qubit"
-                        end
+                if value and (not is_dead_cell(value)) and value['patch_type'] ~= 'DistillationQubit' then
+                    
+                    local connections = {0, 0, 0, 0};
+                    if stritch_border(value['edges']['Top'], value['patch_type']) then connections[1] = 1 end
+                    if stritch_border(value['edges']['Bottom'], value['patch_type']) then connections[2] = 1 end
+                    if stritch_border(value['edges']['Left'], value['patch_type']) then connections[3] = 1 end
+                    if stritch_border(value['edges']['Right'], value['patch_type']) then connections[4] = 1 end
+
+                    local name = string.format("latticesurgery:routing_%i_%s",t%12+1, array_to_s(connections))
+                    if is_dead_cell(value) then
+                        name = "latticesurgery:dead_cell"
+                    elseif value['patch_type'] == 'DistillationQubit' then
+                        name = string.format("latticesurgery:distillation_%s", array_to_s(connections))
+                    elseif value['patch_type'] == 'Qubit' then
+                        name = string.format("latticesurgery:qubit_%s", array_to_s(connections))
+                    end
 
                     local position = add_vectors(starting_point, { x = r, y = t, z = c })
                     minetest.place_node(position,  { name = name })
@@ -54,27 +92,76 @@ end
 
 NUM_ROUTING_COLOURS = 12
 
-for i = 1, 12, 1 do
-    minetest.register_node(string.format("latticesurgery:routing_%i",i), {
-        description = string.format("Routing Volume color variation %i",i),
-        tiles = {string.format("routing_%i.png",i)},
-        drawtype = "glasslike",
+
+
+for j = 0, 15, 1 do
+    -- i to bit string
+    local bitstring = {
+        math.floor(j / 8) % 2,
+        math.floor(j / 4) % 2,
+        math.floor(j / 2) % 2,
+        math.floor(j / 1) % 2};
+
+    minetest.register_node(string.format("latticesurgery:qubit_%s", array_to_s(bitstring)), {
+        description = string.format("Qubit %s", array_to_s(bitstring)),
+        tiles = {"qubit.png"},
+        drawtype = "nodebox",
+        node_box = {
+            type = "connected",
+            drawtype = "nodebox",
+            fixed = {
+                -3/8 - bitstring[1] * 1/8, 
+                -4/8, 
+                -3/8 - bitstring[3] * 1/8, 
+                3/8 + bitstring[2] * 1/8,
+                4/8,
+                3/8 + bitstring[4] * 1/8},
+        },
         groups = {cracky = 1, falling_node=2}
     })
+
+    minetest.register_node(string.format("latticesurgery:distillation_%s", array_to_s(bitstring)), {
+        description = string.format("Distillation volume", array_to_s(bitstring)),
+        tiles = {"distillation.png"},
+        drawtype = "nodebox",
+            node_box = {
+                type = "connected",
+                drawtype = "nodebox",
+                fixed = {
+                    -3/8 - bitstring[1] * 1/8, 
+                    -4/8, 
+                    -3/8 - bitstring[3] * 1/8, 
+                    3/8 + bitstring[2] * 1/8,
+                    4/8,
+                    3/8 + bitstring[4] * 1/8},
+            },
+        groups = {cracky = 1, falling_node=2}
+    })
+
+    for i = 1, 12, 1 do
+        minetest.register_node(string.format("latticesurgery:routing_%i_%s",i, array_to_s(bitstring)), {
+            description = string.format("Routing Volume color variation %i %s",i, array_to_s(bitstring)),
+            tiles = {string.format("routing_%i.png",i)},
+            drawtype = "nodebox",
+            node_box = {
+                type = "connected",
+                drawtype = "nodebox",
+                fixed = {
+                    -3/8 - bitstring[1] * 1/8, 
+                    -3/8, 
+                    -3/8 - bitstring[3] * 1/8, 
+                    3/8 + bitstring[2] * 1/8,
+                    3/8,
+                    3/8 + bitstring[4] * 1/8},
+            },
+            groups = {cracky = 1, falling_node=2}
+        })
+    end
 end
 
-
-minetest.register_node("latticesurgery:distillation", {
-    description = "Distillation volume",
-    tiles = {"distillation.png"},
-    drawtype = "glasslike",
-    groups = {cracky = 1,  falling_node=2}
-})
-
-
-minetest.register_node("latticesurgery:qubit", {
-    description = "Qubit",
-    tiles = {"qubit.png"},
+minetest.register_node("latticesurgery:dead_cell", {
+    description = "Dead Cell",
+    tiles = {"dead.png"},
     drawtype = "glasslike",
     groups = {cracky = 1, falling_node=2}
 })
